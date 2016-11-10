6033cb20e9d48e90fb988c76b6eb2857966156cf
Gpsi <gps-i@foxmail.com>
2016-11-10 20:59:09 +0800

    /测试

--- app/src/main/java/com/gpsi/gdeme/MainActivity.java
+++ app/src/main/java/com/gpsi/gdeme/MainActivity.java
@@ -2,6 +2,8 @@ package com.gpsi.gdeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //123
     }
+
+    public void oneCl(View view){
+        Toast.makeText(MainActivity.this, "就是吐一下!", Toast.LENGTH_SHORT).show();
+    }
+
 }
