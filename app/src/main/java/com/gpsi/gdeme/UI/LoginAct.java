499f0675156098aa546c02e24b92179ad9e47833
Gpsi <gps-i@foxmail.com>
2016-11-30 22:36:07 +0800

    登陆注册点击选择，获取验证码倒计时

--- app/src/main/java/com/gpsi/gdeme/UI/LoginAct.java
+++ app/src/main/java/com/gpsi/gdeme/UI/LoginAct.java
@@ -5,6 +5,7 @@ import android.app.usage.UsageEvents;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -23,6 +24,7 @@ import com.gpsi.gdeme.R;
 import com.gpsi.gdeme.api.ApiContants;
 import com.gpsi.gdeme.bean.LoginBean;
 import com.gpsi.gdeme.utils.MessageEvent;
+import com.gpsi.gdeme.widget.WithDelEditText;
 
 import org.greenrobot.eventbus.EventBus;
 import org.xutils.common.Callback;
@@ -35,40 +37,67 @@ import org.xutils.x;
  */
 public class LoginAct extends Activity implements View.OnClickListener {
 
-    LinearLayout lin_login;
-    LinearLayout lin_register;
+    LinearLayout lin_login_click;
+    LinearLayout lin_register_click;
     View include_log;
     View include_register;
     Button btn_login;
+    Button register_getcode;
+    WithDelEditText register_et_phone;
+    WithDelEditText register_et_code;
 
     EditText editText_login_name;
     EditText editText_login_psw;
+    TextView login_reg_tt;
+    TextView login_log_tt;
+    TextView register_protocol;
+    View login_underscore;
+    View register_underscore;
 
-    TextView register_next;
-    View underscore;
-
+    private CountDownTimer mTimer;
     private static final String TAG = "Gpsi";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        lin_login = (LinearLayout) findViewById(R.id.login_log_click);
-        lin_login.setOnClickListener(this);
-        lin_register = (LinearLayout) findViewById(R.id.login_register_click);
-        lin_register.setOnClickListener(this);
+        lin_login_click = (LinearLayout) findViewById(R.id.login_log_click);
+        lin_login_click.setOnClickListener(this);
+        lin_register_click = (LinearLayout) findViewById(R.id.login_register_click);
+        lin_register_click.setOnClickListener(this);
         include_log = findViewById(R.id.login_log_vib);
         include_register = findViewById(R.id.login_register_vib);
         btn_login = (Button) findViewById(R.id.btn_login_send);
         btn_login.setOnClickListener(this);
+        register_getcode = (Button) findViewById(R.id.register_getcode);
+        register_getcode.setOnClickListener(this);
         editText_login_name = (EditText) findViewById(R.id.editText_login_name);
         editText_login_psw = (EditText) findViewById(R.id.editText_login_psw);
 
-        underscore = findViewById(R.id.login_underscore);
+        login_underscore = findViewById(R.id.login_underscore);
+        register_underscore = findViewById(R.id.register_underscore);
+        login_reg_tt = (TextView) findViewById(R.id.login_reg_tt);
+        login_log_tt = (TextView) findViewById(R.id.login_log_tt);
+        register_et_phone = (WithDelEditText) findViewById(R.id.register_et_phone);
+        register_et_code = (WithDelEditText) findViewById(R.id.register_et_code);
 
-        register_next = (TextView) findViewById(R.id.register_next);
-        register_next.setOnClickListener(this);
+        register_protocol = (TextView) findViewById(R.id.register_protocol);
+        register_protocol.setOnClickListener(this);
 //        EventBus.getDefault().register(this);
+        mTimer = new CountDownTimer(50000, 100) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                register_getcode.setText((millisUntilFinished / 1000) + "秒后重新获取");
+                register_getcode.setBackgroundColor(0xe4e5e4);
+                register_getcode.setClickable(false);
+            }
+
+            @Override
+            public void onFinish() {
+                register_getcode.setEnabled(true);
+                register_getcode.setText("点击获取验证码");
+            }
+        };
 
     }
 
@@ -80,33 +109,44 @@ public class LoginAct extends Activity implements View.OnClickListener {
             case R.id.login_log_click:
                 if (log_isvis == 8) {
                     include_log.setVisibility(View.VISIBLE);
-                    underscore.setBackgroundColor(Color.blue(55));
+                    change(1);
                     include_register.setVisibility(View.GONE);
                 }
                 break;
             case R.id.login_register_click:
                 if (register_isvis == 8) {
                     include_register.setVisibility(View.VISIBLE);
+                    change(2);
                     include_log.setVisibility(View.GONE);
                 }
                 break;
-            case R.id.register_next:
+            case R.id.register_protocol:
                 //注册协议
-
+                Toast.makeText(LoginAct.this, "注册协议", Toast.LENGTH_LONG).show();
                 break;
             case R.id.btn_login_send:
                 //登陆按钮点击
                 login();
 
                 break;
-
             case R.id.btn_register_send:
                 //注册
-
-
+                register();
+                break;
+            case R.id.register_getcode:
+                //获取验证码
+                getVerifyCode();
+                restart();
                 break;
 
         }
+    }
+
+    /**
+     * 注册
+     */
+    private void register() {
+
     }
 
 
@@ -176,9 +216,54 @@ public class LoginAct extends Activity implements View.OnClickListener {
         }
     }
 
+    /**
+     * 改变下划线颜色,1:登陆,2:注册
+     *
+     * @param i
+     */
+    public void change(int i) {
+        if (i == 2) {
+            login_log_tt.setTextColor(Color.BLACK);
+            login_underscore.setBackgroundColor(Color.WHITE);
+            login_reg_tt.setTextColor(Color.RED);
+            register_underscore.setBackgroundColor(Color.RED);
+        } else {
+            login_log_tt.setTextColor(Color.RED);
+            login_underscore.setBackgroundColor(Color.RED);
+            login_reg_tt.setTextColor(Color.BLACK);
+            register_underscore.setBackgroundColor(Color.WHITE);
+
+        }
+
+
+    }
+
+    /**
+     * 开始倒计时
+     */
+    public void restart(){
+        mTimer.start();
+    }
+
+    /**
+     * 结束倒计时
+     */
+    public void oncancel(){
+        mTimer.cancel();
+    }
+
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
 //        EventBus.getDefault().unregister(this);
+    }
+
+    /**
+     * 获取验证码
+     */
+    public void getVerifyCode() {
+        String code = register_et_phone.getText().toString();
+        Log.i(TAG,code);
     }
 }
